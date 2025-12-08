38216ec6f9c928dc1271649d426bd837d958d170
Aryan8872 <gamersimpact778@gmail.com>
2025-12-08 11:28:23 +0545

    build error fixes

--- app/(root)/blogs/page.tsx
+++ app/(root)/blogs/page.tsx
@@ -1,7 +1,9 @@
 import { BlogList } from "./BlogList";
 
 export default async function Blogs() {
-  const blogs = await fetch("http://localhost:8080/blog/all");
+  const blogs = await fetch(
+    `${process.env.NEXT_PUBLIC_RESOLVED_API_BASE_URL}/blog/all`
+  );
   const data = await blogs.json();
   const blogsData = data.data;
   return (
